66aaefe22d3603412ef7dcdf9fa3901533a738b6
Rui Cheng <ktei2008@gmail.com>
2013-08-10 16:22:31 +1000

    basic code for testing

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require "sinatra/base"
+
+class MyApp < Sinatra::Base
+
+  set :title, 'Rui Space!'
+
+  get '/' do
+    settings.title
+  end
+  # $0 is the executed file
+  # __FILE__ is the current file
+  run! if __FILE__ == $0
+end
